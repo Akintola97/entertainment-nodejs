1434233b9b80dcc732a8ad5e9ced358b1e144f83
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-09-23 14:31:57 -0500

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,47 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { useAuth } from "../AuthContext";
+
+// const Navbar = () => {
+//   const { user, logout } = useAuth();
+
+//   return (
+//     <div className="fixed top-0 left-0 right-0 bg-black w-full h-[8vh] flex text-white items-center z-10">
+//       <div className="w-full">
+//         <h2 className="p-5 text-[3vmin]">Marvel Heroes</h2>
+//       </div>
+//       <div className="w-full flex flex-wrap items-center justify-end p-2">
+//         {user ? (
+//           <div className="flex items-center">
+//             <p className="p-2 sm:p-5 text-[2.3vmin]">Hi, {user}</p>
+//             <Link to="/heropage">
+//               <p className="p-2 sm:p-5 text-[2.3vmin]">Home</p>
+//             </Link>
+//             <Link to="/saved">
+//               <p className="p-2 sm:p-5 text-[2.3vmin]">Saved</p>
+//             </Link>
+//             <button
+//               onClick={logout}
+//               className="bg-red-500 hover:bg-red-600 text-white py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out text-[2.5vmin]"
+//             >
+//               Logout
+//             </button>
+//           </div>
+//         ) : (
+//           <Link to="/">
+//             <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out text-[2.5vmin]">
+//               Login
+//             </button>
+//           </Link>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../AuthContext";
@@ -13,20 +57,23 @@ const Navbar = () => {
       <div className="w-full flex flex-wrap items-center justify-end p-2">
         {user ? (
           <div className="flex items-center">
-            <p className="p-2 sm:p-5">Hi, {user}</p>
+            <p className="text-[2.2vmin] p-2 md:p-5">Hi, {user}</p>
+            <Link to="/heropage">
+              <p className="text-[2.2vmin]">Home</p>
+            </Link>
             <Link to="/saved">
-              <p className="p-2 sm:p-5">Saved</p>
+              <p className="text-[2.2vmin] p-2 md:p-5">Saved</p>
             </Link>
             <button
               onClick={logout}
-              className="bg-red-500 hover:bg-red-600 text-white py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out text-sm sm:text-base"
+              className="bg-red-500 hover:bg-red-600 text-white py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out text-[2.4vmin]"
             >
               Logout
             </button>
           </div>
         ) : (
           <Link to="/">
-            <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out text-sm sm:text-base">
+            <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out text-[2.2vmin]">
               Login
             </button>
           </Link>
