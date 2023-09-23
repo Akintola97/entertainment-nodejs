7cf32ae68f3c832031aadbb5ce6eccdfcde4c99d
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-09-22 22:49:54 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,40 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import HeroPage from "./pages/HeroPage";
-import axios from "axios";
-import AuthProvider from "./AuthContext";
-import Saved from "./components/Saved";
-
-
-
-
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import HeroPage from './pages/HeroPage';
+import axios from 'axios';
+import AuthProvider, { useAuth } from './AuthContext';
+import Saved from './components/Saved';
 
 axios.defaults.withCredentials = true;
 
-function App() {
+// Create a custom Route component for protected routes
+function PrivateRoute({ element }) {
+  const { user } = useAuth();
 
+  return user ? element : <Navigate to="/" />;
+}
+
+function App() {
   return (
     <div>
       <AuthProvider>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/heropage' element={<HeroPage />} />
-          <Route path='/saved' element={<Saved />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/heropage"
+            element={<PrivateRoute element={<HeroPage />} />}
+          />
+          <Route
+            path="/saved"
+            element={<PrivateRoute element={<Saved />} />}
+          />
         </Routes>
-        </AuthProvider>
+      </AuthProvider>
     </div>
   );
 }
