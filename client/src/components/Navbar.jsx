91afde17af8cf4da2b4b1e94f55f8b1fc33c8dc6
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-09-23 20:21:26 -0500

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bg-black w-full h-[8vh] flex text-white items-center z-10">
       <div className="w-full">
+        <Link to = '/heropage'>
         <h2 className="p-5 text-[3vmin]">Marvel Heroes</h2>
+        </Link>
       </div>
       <div className="w-full flex flex-wrap items-center justify-end p-2">
         {user ? (
