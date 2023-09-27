4878f004776c3510daea996076281e014d210bb6
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-09-27 12:34:14 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ mongoose.connect(mongodb).then(() => {
 // });
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 
